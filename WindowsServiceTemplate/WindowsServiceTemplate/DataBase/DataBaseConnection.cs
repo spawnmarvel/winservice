5b504ffedde7c8edd3da05b51b07b446de842faa
Jan Espen Kleivane <espenkleivane@gmail.com>
2018-01-27 00:11:42 +0100

    packages json, npgsql

---
+++ WindowsServiceTemplate/WindowsServiceTemplate/DataBase/DataBaseConnection.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceTemplate.DataBase
+{
+    class DataBaseConnection
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(typeof(DataBaseConnection));
+        private static NpgsqlConnection conn;
+        //private static NpgsqlDataAdapter da;
+        //private static NpgsqlCommand cmd;
+        //PostgeSQL-style connection string
+        private static string conString;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public DataBaseConnection()
+        {
+            setUpConnection(readJsonMakeConnectionString());
+        }
+        /// <summary>
+        /// set up connection
+        /// </summary>
+        /// <param name="auth"></param>
+        public void setUpConnection(string auth)
+        {
+            logger.Info("Set up connection string json");
+            string[] rv = auth.Split(';');
+            string host = rv[0];
+            string database = rv[1];
+            int port = Convert.ToInt32(rv[2]);
+            string user = rv[3];
+            string pass = rv[4];
+            conString = String.Format("Server={0};Port={1};" + "User Id={2};Password={3};Database={4};", host, port, user, pass, database);
+
+        }
+        /// <summary>
+        /// get connection
+        /// </summary>
+        /// <returns></returns>
+        public NpgsqlConnection getConnection()
+        {
+            return conn;
+        }
+        /// <summary>
+        /// open db
+        /// </summary>
+        /// <returns></returns>
+        public bool openDb()
+        {
+            bool status = false;
+            try
+            {
+                conn = new NpgsqlConnection(conString);
+                conn.Open();
+                status = true;
+                logger.Info("Db is open");
+            }
+            catch (Exception msg)
+            {
+                logger.Debug(msg);
+            }
+            return status;
+        }
+
+        /// <summary>
+        /// close db
+        /// </summary>
+        /// <returns></returns>
+        public bool closeDb()
+        {
+            bool status = false;
+            try
+            {
+
+                conn.Close();
+                status = true;
+                logger.Info("Db is closed");
+            }
+            catch (Exception msg)
+            {
+                logger.Debug(msg);
+            }
+            return status;
+        }
+
+        /// <summary>
+        /// read json, format {"host": "localhost","database": "my_db","port": 9999,"user": "my_user","pass" : "my_password", "description" : "you get the picture"}
+        /// </summary>
+        /// <returns></returns>
+        public string readJsonMakeConnectionString()
+        {
+
+            string jAuth = "";
+            bool status = false;
+            try
+            {
+
+                using (StreamReader file = File.OpenText("auth.json"))
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    JObject authFile = (JObject)JToken.ReadFrom(reader);
+                    //logger.Debug(reader.TokenType + " " + reader.ValueType + " " + reader.Value);
+                    foreach (KeyValuePair<string, JToken> keyValue in authFile)
+                    {
+                        //get the values
+                        //logger.Debug(keyValue.Value.ToString());
+                        jAuth += keyValue.Value.ToString() + ";";
+                        status = true;
+                    }
+
+                }
+
+            }
+            catch (FileNotFoundException msg)
+            {
+                logger.Debug(msg);
+            }
+            logger.Debug("Read json file status: " + status);
+            return jAuth;
+        }
+    }
+}
--- WindowsServiceTemplate/WindowsServiceTemplate/Worker/ServiceWorker.cs
+++ WindowsServiceTemplate/WindowsServiceTemplate/Worker/ServiceWorker.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WindowsServiceTemplate.DataBase;
 namespace WindowsServiceTemplate.Worker
 {
     class ServiceWorker : IServiceWorker
     {
         private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(typeof(ServiceWorker));
-
+        private DataBaseConnection dbCon;
         private string name;
 
         public ServiceWorker(string name)
         {
             this.name = name;
+            //dbCon = new DataBaseConnection();
         }
 
 
         public void doWork()
         {
             logger.Info("Start do work " + Name);
+            //dbCon.openDb();
+            //dbCon.closeDb();
+
         }
 
         public void stopWork()
